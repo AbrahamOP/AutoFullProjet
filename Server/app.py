148bf554644f6cdfba2d5b6040a55370b15df6ac
Antoine .P <130924909+AbrahamOP@users.noreply.github.com>
2024-05-17 01:20:35 +0200

    update app.py

--- Server/app.py
+++ Server/app.py
@@ -16,7 +16,7 @@ PROXMOX_PASSWORD = os.getenv('PROXMOX_PASSWORD')
 MIN_VM_ID = 200
 
 def get_proxmox_token():
-    """Authentification à l'API Proxmox pour obtenir le ticket et le CSRF token."""
+
     response = requests.post(
         f'https://{PROXMOX_IP}/api2/json/access/ticket',
         data={'username': PROXMOX_USERNAME, 'password': PROXMOX_PASSWORD},
@@ -26,7 +26,7 @@ def get_proxmox_token():
     return response.json()['data']['ticket'], response.json()['data']['CSRFPreventionToken']
 
 def get_next_vmid(ticket, csrf_token):
-    """Récupération du prochain VMID disponible."""
+
     headers = {'Cookie': f'PVEAuthCookie={ticket}', 'CSRFPreventionToken': csrf_token}
     response = requests.get(
         f'https://{PROXMOX_IP}/api2/json/cluster/resources?type=vm',
@@ -39,7 +39,7 @@ def get_next_vmid(ticket, csrf_token):
     return max([MIN_VM_ID] + existing_vmids) + 1
 
 def validate_data(data):
-    """Validation des données d'entrée."""
+
     allowed_memory_values = {512, 1024, 2048, 4096, 8192, 16384}
     allowed_os_templates = {'ubuntu-20.04', 'ubuntu-22.04', 'debian-11', 'debian-10'}
 
@@ -61,7 +61,7 @@ def validate_data(data):
     return data
 
 def create_resource(resource_type, data):
-    """Crée une VM ou un CT en utilisant l'API Proxmox."""
+
     try:
         validate_data(data)
         ticket, csrf_token = get_proxmox_token()
@@ -103,7 +103,7 @@ def create_ct():
 
 @app.route('/')
 def index():
-    """Renvoie la page d'accueil."""
+
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
